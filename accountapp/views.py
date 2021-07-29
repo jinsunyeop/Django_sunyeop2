6fbd47d4d97caadf6b190c162ac1696e185b4726
진선엽 <sunyeop2@jinseon-yeob-ui-MacBookAir.local>
2021-07-29 14:43:35 +0900

    07/29/02:43

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.forms import UserUpdateForm
 
 
 class AccountCreateView(CreateView):
@@ -13,3 +15,26 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('basicapp:basic')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+
+    model = User
+    form_class = UserUpdateForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('basicapp:basic')
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
