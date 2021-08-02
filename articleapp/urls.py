2d5456f52be868bfbe835b05272795cb0005add8
진선엽 <sunyeop2@jinseon-yeob-ui-MacBookAir.local>
2021-08-02 17:43:21 +0900

    08/02

---
+++ articleapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from articleapp.views import ArticleCreateView, ArticleDetailView
+
+app_name='articleapp'
+
+
+urlpatterns = [
+
+    path('create/',ArticleCreateView.as_view(),name='create'),
+    path('detail/<int:pk>',ArticleDetailView.as_view(),name='detail')
+
+
+
+]
